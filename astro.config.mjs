50346ca915051cd905f13ad85fb352a4891f8a86
Kuldas <jsem@stepankulik.cz>
2022-11-23 20:40:33 +0100

    add Netlify CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,32 @@
 import { defineConfig } from 'astro/config';
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections:  [
+          {
+            name: 'posts',
+            label: 'Blog posts',
+            folder: 'src/pages/posts',
+            create: true,
+            delete: true,
+            fields: [
+            { name: 'title', widget: 'string', label: 'Post title' },
+            { name: 'body', widget: 'markdown', label: 'Post body'},
+            ],
+          },
+        ],
+      },
+    }),
+    preact()]
 });
